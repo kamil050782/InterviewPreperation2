8f0f8d241f920ab5985ce41586a276fa7f8446e5
husey <kamil82@mail.ru>
2022-05-04 23:36:14 -0400

    Task 10 done

---
+++ src/InterviewPreparation/Task10.java
@@ -0,0 +1,24 @@
+package InterviewPreparation;
+
+public class Task10 {
+
+    /*if ascend true
+    int[]a={1,2,3,9,4,5,6,7};
+
+    output: biggest number 9
+
+    */
+    public static void main(String[] args) {
+
+
+        int[] a = {1, 2, 3, 9, 4, 5, 6, 7};
+
+        int max = Integer.MIN_VALUE;
+        for (int i = 0; i < a.length; i++) {
+            if (a[i] > max) {
+                max = a[i];
+            }
+        }
+        System.out.println(max);
+    }
+}
