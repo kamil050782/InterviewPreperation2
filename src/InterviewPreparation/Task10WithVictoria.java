2eca87f38b88b9f6e524413ebe84dc27aa0adaf4
husey <kamil82@mail.ru>
2022-05-04 23:42:05 -0400

    Task 10 With Victoria

---
+++ src/InterviewPreparation/Task10WithVictoria.java
@@ -0,0 +1,17 @@
+package InterviewPreparation;
+
+import java.util.Arrays;
+
+public class Task10WithVictoria {
+
+         /*if ascend true
+    int[]a={1,2,3,4,5,6,7};
+    */
+        public static void main(String[] args) {
+            int[]a={1,2,3,4,5,6,7};
+            int[]b={1,2,3,4,5,6,7};
+            Arrays.sort(a);
+
+            System.out.println(Arrays.equals(a, b));
+    }
+}
