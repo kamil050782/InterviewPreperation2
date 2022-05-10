7b21f73bea482f8bae40dba0f6ab504da69d9723
husey <kamil82@mail.ru>
2022-05-09 23:47:37 -0400

    Task 14 With Victoria

---
+++ src/InterviewPreparation/Task14.java
@@ -0,0 +1,21 @@
+package InterviewPreparation;
+
+public class Task14 {
+    /*Use String
+    int a=5;
+
+if number is odd output will be odd, if number is even output will be even
+
+     */
+
+    public static String oddEven(int num){
+       return num%2==0?"even":"odd";
+
+    }
+
+    public static void main(String[] args) {
+        int a=5;
+        System.out.println(oddEven(a));
+    }
+
+}
