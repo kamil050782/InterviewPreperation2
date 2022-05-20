cbe6925db7a60147278a1c4c72d730f63c90d690
husey <kamil82@mail.ru>
2022-05-19 23:47:52 -0400

    Task 21

---
+++ src/InterviewPreparation/Task21Factorial.java
@@ -0,0 +1,20 @@
+package InterviewPreparation;
+
+public class Task21Factorial {
+    public static int factorial(int num){
+        int factorial=1;
+
+        for (int i = 1; i <= num; i++) {
+            factorial=factorial*i;
+
+
+        }
+        return factorial;
+    }
+
+    public static void main(String[] args) {
+        int num=5;
+        System.out.println(factorial(num));
+    }
+
+}
