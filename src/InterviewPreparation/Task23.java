f05fee1eb130bc1c424ba3ee420a65265b6da56b
husey <kamil82@mail.ru>
2022-05-24 23:43:52 -0400

    Task 23WithVictoria

---
+++ src/InterviewPreparation/Task23.java
@@ -0,0 +1,31 @@
+package InterviewPreparation;
+
+public class Task23 {
+    /* int num=-1234556;
+    -6554321
+    */
+    public static String reverseNumber(int num) {
+        String reserve = String.valueOf(num);//String reserve=""+num;
+        String result = "";
+
+        if (reserve.charAt(0) == '-') {
+            result += reserve.charAt(0);
+            for (int i = reserve.length() - 1; i >= 1; i--) {
+                result += reserve.charAt(i);
+
+            }
+        } else {
+            for (int i = reserve.length() - 1; i >= 0; i--) {
+                result += reserve.charAt(i);
+
+            }
+
+        }
+        return result;
+    }
+
+    public static void main(String[] args) {
+        int num=-1234556;
+        System.out.println(reverseNumber(num));
+    }
+}
