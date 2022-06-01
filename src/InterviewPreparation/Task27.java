d5394bc3121f3c1992a0e15ab19537abf16e68ea
husey <kamil82@mail.ru>
2022-06-01 00:10:14 -0400

    Task 27WithMaharramAndVictoria

---
+++ src/InterviewPreparation/Task27.java
@@ -0,0 +1,32 @@
+package InterviewPreparation;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+public class Task27 {
+    /*27 frequency
+    String str="AABBDDDC";
+    Map<Character, Integer> map = new LinkedHashMap<>();
+    output
+    {A=2, B=2, D=3, C=1}
+    */
+    public static Map<Character, Integer> frequency(String str) {
+        Map<Character, Integer> map = new LinkedHashMap<>();
+        int count = 0;
+        for (int i = 0; i < str.length(); i++) {
+            if (map.containsKey(str.charAt(i))) {
+                count=map.get(str.charAt(i))+1;
+                map.put(str.charAt(i),count);
+
+            } else {
+                map.put(str.charAt(i),1);
+            }
+        }
+        return map;
+    }
+
+    public static void main(String[] args) {
+        String str="AABBDDDC";
+        System.out.println(frequency(str));
+    }
+}
