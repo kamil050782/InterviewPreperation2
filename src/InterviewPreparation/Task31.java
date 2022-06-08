06872c7a82b375c41b7032c119a746481ee324fa
husey <kamil82@mail.ru>
2022-06-07 23:15:58 -0400

    Task 32WithMaharramAndVictoria

--- src/InterviewPreparation/Task31.java
+++ src/InterviewPreparation/Task31.java
@@ -2,9 +2,11 @@ package InterviewPreparation;
 
 public class Task31 {
 
-     //Given 2 arrays of ints, a and b, return true if
-    //they have the same first element or they have the same last element.
-    //Both arrays will be length 1 or more.
+     /*Given 2 arrays of ints, a and b, return true if
+    they have the same first element or they have the same last element.
+    Both arrays will be length 1 or more.
+
+      */
     public static boolean compareArrays(int[]arr,int[]arr1){
         if (arr[0]==arr1[0] || arr[arr.length-1]==arr1 [arr1.length-1]){
             return true;
@@ -15,7 +17,7 @@ public class Task31 {
 
     public static void main(String[] args) {
         int[]arr={1,2,3,4,5};
-        int[]arr1={1,2,4,8,5};
+        int[]arr1={1,2,4,8,9};
         System.out.println(compareArrays(arr,arr1));
     }
 
---
+++ src/InterviewPreparation/Task32.java
@@ -0,0 +1,25 @@
+package InterviewPreparation;
+
+import java.util.Arrays;
+
+public class Task32 {
+
+    /*Create a function that counts the number of syllables a word has. Each syllable is
+    separated with a dash "-" Examples
+    numberSyllables("buf-fet") ➞ 2
+
+    String q="buf-dt-rr"; >> 3
+
+     */
+    public static int syllables(String str){
+       return  str.split("-").length;
+//        String[]name=str.split("-");
+//        int result=name.length;
+//        return result;
+    }
+
+    public static void main(String[] args) {
+        String q="buf-dt-rr";
+        System.out.println(syllables(q));
+    }
+}
