f03c736610ee4305df887a7f7668f9e536845dd3
husey <kamil82@mail.ru>
2022-06-14 23:48:30 -0400

    Task WithMaharramAndVictoriaTask43 new

---
+++ src/InterviewPreparation/Task43.java
@@ -0,0 +1,26 @@
+package InterviewPreparation;
+
+public class Task43 {
+    /*43 Perfect number is the sum of numbers that perfectNum divided by sum
+    int perfectNum =28;
+eger regeme bolunen regemlerin cemi onun ozune beraberdirse perfect numberdi
+
+     */
+    public static boolean perfectNumber(int num){
+        int result=0;
+        for (int i =1;i<num;i++){
+         if (num%i==0){
+             result+=i;
+         }
+
+        }
+        return num==result;
+    }
+
+    public static void main(String[] args) {
+     int num =28;
+        System.out.println(perfectNumber(num));
+
+    }
+
+}
