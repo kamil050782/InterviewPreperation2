81bc076b9affa82a7850d4f4e88a255825f71683
husey <kamil82@mail.ru>
2022-06-14 23:26:58 -0400

    Task WithMaharramAndVictoriaTask42 new

---
+++ src/InterviewPreparation/Task42.java
@@ -0,0 +1,31 @@
+package InterviewPreparation;
+
+public class Task42 {
+    /*42  reverse   USE STRINGBUFFER
+    int num = -123;
+    -321
+    */
+    public static int stringBuffer(int num) {
+        String temp = "" + num;
+        StringBuffer stringBuffer = new StringBuffer();
+        if (temp.charAt(0) == '-') {
+            stringBuffer.append(temp.charAt(0));
+            for (int i = temp.length() - 1; i > 0; i--) {
+                stringBuffer.append(temp.charAt(i));
+            }
+        } else {
+            for (int i = temp.length() - 1; i >= 0; i--) {
+                stringBuffer.append(temp.charAt(i));
+            }
+
+        }
+        return Integer.parseInt(String.valueOf(stringBuffer));
+
+    }
+
+        public static void main (String[]args){
+            int num = -123;
+            System.out.println(stringBuffer(num));
+        }
+
+    }
