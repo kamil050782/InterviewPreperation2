8f0c43adc4d11aa880c4e1a12a1c6145574fd33b
husey <kamil82@mail.ru>
2022-06-16 23:23:23 -0400

    Task WithMaharramAndVictoriaTask44

---
+++ src/InterviewPreparation/Task44_2_String.java
@@ -0,0 +1,24 @@
+package InterviewPreparation;
+
+public class Task44_2_String {
+//     ---WHAT IS OUTPUT----
+//    String S1 = "Nisum";
+//    String S2 = new String(S1);
+//    String S3 ="Nisum";
+//        System.out.println((S1.equals(S2)));
+//        System.out.println(S1==S2);
+//        System.out.println((S3==S1));
+public static void main(String[] args) {
+    String S1 = "Nisum";
+    String S2 = new String(S1);
+    String S3 ="Nisum";
+       System.out.println((S1.equals(S2)));
+      System.out.println(S1==S2);
+       System.out.println((S3==S1));
+       String S4="";
+       String S5=S1+S4;
+    System.out.println(S5==S1);
+
+
+}
+}
