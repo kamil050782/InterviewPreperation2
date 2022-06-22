34e7f88fc27282c309bce23604aeee2f898d89c3
husey <kamil82@mail.ru>
2022-06-21 23:21:32 -0400

    Task WithMaharramAndVictoriaTask46

---
+++ src/InterviewPreparation/Task46.java
@@ -0,0 +1,31 @@
+package InterviewPreparation;
+
+public class Task46 {
+    /*
+
+    String a="aaa10ss20bb30";
+
+    output > 60
+    */
+    public static int tempNumbers( String str){
+        String temp= "0";
+        int sum=0;
+        for (int i = 0; i < str.length(); i++) {
+            if (Character.isDigit(str.charAt(i))){
+                temp+=str.charAt(i);
+            }else {
+                sum+=Integer.parseInt(temp);
+                temp="0";
+            }
+
+        }
+        return sum+Integer.parseInt(temp);
+
+    }
+
+    public static void main(String[] args) {
+        String str="aaa10ss20bb30";
+        System.out.println(tempNumbers(str));
+    }
+
+}
