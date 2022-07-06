10254054ae59492f4f3de9db336634e3c59e7f2b
husey <kamil82@mail.ru>
2022-07-05 22:52:42 -0400

    Commit from Kamil

---
+++ src/InterviewPreparation/Task56Kamil.java
@@ -0,0 +1,28 @@
+package InterviewPreparation;
+
+public class Task56Kamil {
+
+    /*
+    Given an array of ints, print true if the array contains a 5 next
+    to a 5 anywhere in the array. If no consecutive 5s or no 5s are
+    detected in your code then print false.
+    nums → [1, 5, 5, 1, 1] → true
+    nums → [1, 8, 5, 5, 0] → true
+    nums → [1, 5, 4, 1, 5] → false
+    nums → [1, 4, 4, 1, 99] → false
+     */
+
+    public static boolean fiveNextFive(int[]arr){
+        for (int i = 0; i < arr.length-1; i++) {
+            if (arr[i]==arr[i+1] ){
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public static void main(String[] args) {
+        int[] arr={1, 5, 5, 1, 1};
+        System.out.println(fiveNextFive(arr));
+    }
+}
