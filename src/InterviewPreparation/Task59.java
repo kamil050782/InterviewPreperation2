4c5f32e1c179d15b7ee4accb4e9d352f618a006c
husey <kamil82@mail.ru>
2022-07-05 23:32:14 -0400

    Solution from Maharram

---
+++ src/InterviewPreparation/Task59.java
@@ -0,0 +1,22 @@
+package InterviewPreparation;
+
+public class Task59 {
+    /*
+    String sentence = "Today is great day";
+    Array [Today, is, great, day]
+    reverse Array[Today, is, great, day] to >  day great is Today
+    */
+    public static String reverseArray(String str){
+        String[] reverse=str.split(" ");
+        String result="";
+        for (int i = reverse.length- 1; i >= 0; i--) {
+            result+=reverse[i]+" ";
+        }
+        return result;
+    }
+
+    public static void main(String[] args) {
+        String sentence = "Today is great day";
+        System.out.println(reverseArray(sentence));
+    }
+}
