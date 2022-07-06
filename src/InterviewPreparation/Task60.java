531d0cb0cf11633e734cb92f5cbafa7c78c017b1
husey <kamil82@mail.ru>
2022-07-05 23:40:09 -0400

    Solution from Victoria Mishchenko

---
+++ src/InterviewPreparation/Task60.java
@@ -0,0 +1,21 @@
+package InterviewPreparation;
+
+public class Task60 {
+    /*
+    String sentence = "Today is great day";
+    yad taerg si yadoT
+    */
+
+    public static String reverseSentence(String str){
+        String reverse="";
+        for (int i = str.length()-1; i >= 0; i--) {
+            reverse+=str.charAt(i);
+        }
+        return reverse+" ";
+    }
+
+    public static void main(String[] args) {
+        String sentence = "Today is great day";
+        System.out.println(reverseSentence(sentence));
+    }
+}
