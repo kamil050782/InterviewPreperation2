84cd44669f8cd114630adce8e7d04c56ff803be6
husey <kamil82@mail.ru>
2022-07-12 23:14:27 -0400

    from QA Lead Victoria

---
+++ src/InterviewPreparation/Task68.java
@@ -0,0 +1,28 @@
+package InterviewPreparation;
+
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
+public class Task68 {
+    /*
+        input
+        int[] num21 = {1,2,4,0,0,5,0};
+        output
+               [5, 4, 2, 1, 0, 0, 0]
+
+               */
+    public static int[]moveZerosAndDescending(int[]arr){
+        int count=0;
+        int[]arr1=new int[arr.length];
+        Arrays.sort(arr);//0001245
+        for (int i = arr.length - 1; i >= 0; i--) {
+           arr1[i]=arr[count++];//542100
+        }
+        return arr1;
+    }
+
+    public static void main(String[] args) {
+        int[] num21 = {1,2,4,0,0,5,0};
+        System.out.println((Arrays.toString(moveZerosAndDescending(num21))));
+    }
+}
