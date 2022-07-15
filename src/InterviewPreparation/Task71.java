0ffa6d636534f9bab01385c43017270e56f69df5
husey <kamil82@mail.ru>
2022-07-14 23:38:51 -0400

    Commit from scrum master Maharram

---
+++ src/InterviewPreparation/Task71.java
@@ -0,0 +1,28 @@
+package InterviewPreparation;
+
+public class Task71 {
+    /*
+    String a="a2b5c7a9f0dafa2s6a8d5a";
+    String str="";
+
+     output       aaaaaa2b5c79f0df2s68d5
+     */
+    public static String moveString(String str){
+        String result="";
+        for (int i = 0; i < str.length(); i++) {
+            if (str.charAt(i)=='a'){
+                result+='a';
+             //   str=str.replaceFirst("a","/");
+
+            }
+        }
+        str=str.replaceAll("a","");
+        result+=str;
+        return result;
+    }
+
+    public static void main(String[] args) {
+        String a="a2b5c7a9f0dafa2s6a8d5a";
+        System.out.println(moveString(a));
+    }
+}
