6d30646983fb897031c9de102486db9cf1b020cc
husey <kamil82@mail.ru>
2022-07-15 00:06:46 -0400

    Group work

---
+++ src/InterviewPreparation/Task72.java
@@ -0,0 +1,24 @@
+package InterviewPreparation;
+
+public class Task72 {
+    /*
+    don't use divide operator
+    int a=11;
+    int b=2;
+    divide a by b and result is =5
+    reminder =1
+    */
+    public static void divideWithoutOperator (int num, int num2){
+        int result=0;
+       while (num>num2){
+           num-=num2;
+           result++;
+       }
+        System.out.println(result);
+        System.out.println(num);
+    }
+
+    public static void main(String[] args) {
+        divideWithoutOperator(11,2);
+    }
+}
