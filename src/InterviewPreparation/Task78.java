e4cb925de200b6b4009599e9fd3819010b4c04fe
husey <kamil82@mail.ru>
2022-07-22 00:08:28 -0400

    From Maharram magic approach with Victoria

---
+++ src/InterviewPreparation/Task78.java
@@ -0,0 +1,38 @@
+package InterviewPreparation;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Task78 {
+    /* USE MAP
+    int a=25;
+    int b=10;
+
+    you have 2 integers
+    and with values assigned
+    how would u add them without + sign
+            result =35;
+    then subtract
+    result=15;
+     */
+    public static void useMap(int num, int num1) {
+        Map<Integer, Integer> map = new HashMap<>();
+        map.put(num, num1);
+        int count = 0;
+        int min=0;
+        for (Map.Entry<Integer, Integer> map1 : map.entrySet()) {
+            count = Math.addExact(map1.getKey(), map1.getValue());
+             min=Math.subtractExact(map1.getKey(),map1.getValue());
+        }
+
+        System.out.println(count);
+        System.out.println(min);
+
+
+    }
+
+    public static void main(String[] args) {
+        useMap(25,10);
+
+    }
+}
