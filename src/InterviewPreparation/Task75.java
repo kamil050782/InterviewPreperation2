4246206a7201cd4519380940e18c6d8582148daa
husey <kamil82@mail.ru>
2022-07-21 22:54:45 -0400

    Git lessons for Maharram

---
+++ src/InterviewPreparation/Task75.java
@@ -0,0 +1,30 @@
+package InterviewPreparation;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class Task75 {
+    /*
+    ArrayList<String> gg = new ArrayList<>(Arrays.asList("three,four", "five","seven"));
+    print out
+    four five seven
+    */
+    public static String removeThree(ArrayList<String>list){
+        String result="";
+        for (int i = 0; i < list.size(); i++) {
+            if (i==0){
+                String[]arr=list.get(i).split(",");
+                result+=arr[i]+" ";
+            }else {
+                result+=list.get(i)+" ";
+            }
+
+        }
+        return result;
+    }
+
+    public static void main(String[] args) {
+        ArrayList<String> gg = new ArrayList<>(Arrays.asList("three,four", "five","seven"));
+        System.out.println(removeThree(gg));
+    }
+}
