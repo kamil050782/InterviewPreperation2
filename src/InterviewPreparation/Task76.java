48b7b2050e999b567089959bc385a5ee6c32e426
husey <kamil82@mail.ru>
2022-07-21 23:18:21 -0400

    From PO Victoria

---
+++ src/InterviewPreparation/Task76.java
@@ -0,0 +1,27 @@
+package InterviewPreparation;
+
+import java.util.Arrays;
+
+public class Task76 {
+    /*
+    Output
+
+    Aa Bb Cc Dd Ee Ff Gg Hh Ii Jj Kk Ll Mm Nn Oo Pp Qq Rr Ss Tt Uu Vv Ww Xx Yy Zz
+[Aa , Bb , Cc , Dd , Ee , Ff , Gg , Hh , Ii , Jj , Kk , Ll , Mm , Nn , Oo , Pp , Qq , Rr , Ss , Tt ]
+
+     */
+    public static String[]alphabet(String str){
+
+        String[]arr=str.split(" ");
+        for (int i = 0; i < arr.length; i++) {
+            arr[i]=arr[i]+" ";
+        }
+        return arr;
+    }
+
+    public static void main(String[] args) {
+        String str="Aa Bb Cc Dd Ee Ff Gg Hh Ii Jj Kk Ll Mm Nn Oo Pp Qq Rr Ss Tt Uu Vv Ww Xx Yy Zz";
+        System.out.println(Arrays.toString(alphabet(str)));
+
+    }
+}
