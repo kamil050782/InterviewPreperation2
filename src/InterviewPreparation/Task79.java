4dcad204f34d0918b012b3bed510a86b60b46895
husey <kamil82@mail.ru>
2022-07-26 23:10:37 -0400

    We cant understand task

---
+++ src/InterviewPreparation/Task79.java
@@ -0,0 +1,34 @@
+package InterviewPreparation;
+
+public class Task79 {
+
+    //Write an efficient program to find the sum of contiguous sub array within a
+    // one-dimensional array of numbers which has the largest sum.
+// int a[]={ -2, -3, 4, -1, -2, 1, 5, -3};
+    //   output 7
+
+    public static int sum(int[] arr) {
+
+
+        int maxFinal = Integer.MIN_VALUE;
+        int maxTemp = 0;
+        for (int i = 0; i < arr.length; i++) {
+            maxTemp = maxTemp + arr[i];
+            if (maxFinal < maxTemp) {
+                maxFinal = maxTemp;
+            }
+            if (maxTemp < 0) {
+                maxTemp = 0;
+            }
+        }
+        return maxFinal;
+
+    }
+
+    public static void main(String[] args) {
+
+
+        int a[] = {-2, -3, 4, -1, -2, 1, 5, -3};
+        System.out.println(sum(a));
+    }
+}
