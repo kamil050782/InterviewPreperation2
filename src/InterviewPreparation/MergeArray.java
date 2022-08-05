73cb2ac2ff0e48fb01680c33b9b68963fde685b2
husey <kamil82@mail.ru>
2022-08-04 23:53:00 -0400

    Solution

--- src/InterviewPreparation/MergeArray.java
+++ src/InterviewPreparation/MergeArray.java
@@ -10,11 +10,12 @@ public class MergeArray {
     output Array
 [1, 2, 3, 4, 5, 6]
  */
-    public static int[]mergeArray(int[]arr,int[]arr2){
+    public static int[]mergeArray(int[]arr,int[]arr1,int[]arr2){
         int count=0;
-        int[]arr3=new int[arr.length+ arr2.length];
+        int[]arr3=new int[arr.length+ arr2.length+arr1.length];
         for (int i = 0; i < arr2.length; i++) {
             arr3[count++]=arr[i];
+            arr3[count++]=arr1[i];
             arr3[count++]=arr2[i];
 
 
@@ -23,8 +24,9 @@ public class MergeArray {
     }
 
     public static void main(String[] args) {
-        int a[]={1,3,5};
-        int b[]={2,4,6};
-        System.out.println(Arrays.toString(mergeArray(a,b)));
+        int a[]={1,4,7};
+        int b[]={2,5,8};
+        int c[]={3,6,9};
+        System.out.println(Arrays.toString(mergeArray(a,b,c)));
     }
 }
